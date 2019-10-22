1f1e14b22a3e20e19b2328d89a1d3e6412a01486
zeng.h <zeng.h@starcart.cn>
2019-10-22 15:08:25 +0800

    Initial commit

---
+++ eureka-server/src/main/java/tiger/Application.java
@@ -0,0 +1,18 @@
+package tiger;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
