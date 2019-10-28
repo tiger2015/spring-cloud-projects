04816ca438e075724fd0ea1fef8858d9118a5660
Zenghu <978689162@qq.com>
2019-10-28 20:54:05 +0800

    添加控制器

---
+++ consume-manager/src/main/java/tiger/ConsumeManagerApp.java
@@ -0,0 +1,12 @@
+package tiger;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeManagerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeManagerApp.class, args);
+    }
+}
